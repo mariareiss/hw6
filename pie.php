f35994a218b6102bf45c81cec66b4f99574ce973
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-11-15 13:44:29 -0600

    Update pie.php

--- pie.php
+++ pie.php
@@ -1,70 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Pie Chart Colors</title>
-    <!-- Include Chart.js library -->
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-</head>
+<meta charset="UTF-8">
+<title>JavaScript Librarie 2 - w3school source</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<style>
+</style>
+<script src="https://www.w3schools.com/lib/w3.js"></script>
 <body>
-    <h1>Pie Chart Colors</h1>
-    <canvas id="chargingTimesChart" width="400" height="200"></canvas>
 
-    <script>
-        const electricVehicles = [
-            {
-                "model": "Tesla Model S",
-                "manufacturer": "Tesla",
-                "year": 2021,
-                "charging_time": 4.5
-            },
-            {
-                "model": "Nissan Leaf",
-                "manufacturer": "Nissan",
-                "year": 2020,
-                "charging_time": 6
-            },
-            {
-                "model": "Chevrolet Bolt EV",
-                "manufacturer": "Chevrolet",
-                "year": 2022,
-                "charging_time": 5.5
-            },
-            // Add more vehicle data as needed
-        ];
+<img class="nature" src="img_snowtops.jpg" width="100%">
+<img class="nature" src="img_mountains.jpg" width="100%">
+<img class="nature" src="img_nature.jpg" width="100%">
 
-        const ctx = document.getElementById('chargingTimesChart').getContext('2d');
-        const chargingTimesChart = new Chart(ctx, {
-            type: 'doughnut',
-            data: {
-                labels: electricVehicles.map(vehicle => vehicle.model),
-                datasets: [{
-                    label: 'Charging Time (hours)',
-                    data: electricVehicles.map(vehicle => vehicle.charging_time),
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.6)',
-                        'rgba(54, 162, 235, 0.6)',
-                        'rgba(255, 206, 86, 0.6)',
-                        // Add more colors if you have more vehicles
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        // Add more colors if you have more vehicles
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-    </script>
+<script>
+w3.slideshow(".nature", 1500);
+</script>
+
 </body>
 </html>
 
